a2c286bf506536bb0ac7d40752c3f3331746be33
Robin Schel <robin@sociossports.nl>
2020-04-06 19:26:25 +0200

    Generate Quotes, also added intervals

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * Initialize all the quote objects
 ***/
 
 var quotes = [
@@ -16,38 +16,44 @@ var quotes = [
     quote: "I think I have learned that the best way to lift one’s self up is to help some one else.",
     source: "Booker T. Washington",
     citation: "The Story of My Life and Work",
-    year: "1900"
+    year: "1900",
+    link: "https://www.amazon.com/dp/1477481079/"
   },
   {
     quote: "It's the possibility of having a dream come true that makes life interesting.",
     source: "Paulo Coelho",
     citation: "The Alchemist",
-    year: "2014"
+    year: "2014",
+    link: "https://www.amazon.com/dp/0061122416"
   },
   {
     quote: "Memories warm you up from the inside. But they also tear you apart.",
     source: "Haruki Murakami",
     citation: "Kafka on the Shore",
-    year: "2006"
+    year: "2006",
+    link: "https://www.amazon.com/dp/1400079276"
   },
   {
     quote: "Get busy living or get busy dying.",
     source: "Stephen King",
     citation: "The Shawshank Redemption",
-    year: "1995"
+    year: "1995",
+    link: "https://www.amazon.com/dp/0751514624"
   },
   {
     quote: "My advice is, never do tomorrow what you can do today. Procrastination is the thief of time.",
     source: "Charles Dickens",
     citation: "David Copperfield",
-    year: "1850"
+    year: "1850",
+    link: "https://www.amazon.com/dp/1479387576"
   }
 ];
 
 
 
 /***
- * `getRandomQuote` function
+ * Gets a random object from the quotes array, random object is generated
+ * by randomNumber
 ***/
 
 
@@ -59,7 +65,22 @@ function getRandomQuote() {
 
 
 /***
- * `printQuote` function
+ * Generates a random HEX color based on possible characters
+***/
+
+
+function getRandomColor() {
+  var possibleCharacters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += possibleCharacters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+/***
+ * Prints the quote to index.html
 ***/
 
 function printQuote() {
@@ -76,12 +97,27 @@ function printQuote() {
   if (getQuote.year)
     quote += '<span class="year">' + getQuote.year + '</span>';
 
+  if (getQuote.link)
+    quote += '<span class="link"><a href="'+ getQuote.link + '" target="_blank">Get the book</a></span>'
+
   quote += '</p>';
   
   document.getElementById('quote-box').innerHTML = quote;
+
+  // Sets a random background color
+  setRandomColor();
+  update();
 }
 
 
+/***
+ * Sets the random HEX color 
+***/
+
+
+function setRandomColor() {
+  document.body.style.backgroundColor = getRandomColor();
+}
 
 /***
  * click event listener for the print quote button
